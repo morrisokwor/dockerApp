a1e21916249bee8690651839dfd6292533816ddd
Morris Okworo <Morris.Okworo@smart.local>
2022-03-16 13:53:52 +0300

    make it restful

--- src/main/java/co/ke/dockerApp/DockerAppApplication.java
+++ src/main/java/co/ke/dockerApp/DockerAppApplication.java
@@ -2,12 +2,22 @@ package co.ke.dockerApp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
+@RequestMapping("")
 public class DockerAppApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DockerAppApplication.class, args);
 	}
 
+	@GetMapping("/")
+	ResponseEntity<?> getStatus(){
+		return ResponseEntity.ok("I am Up And Running");
+	}
 }
